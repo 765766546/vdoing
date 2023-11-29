7ebb278f5aa1a6581c43e2088d7a443335d155c7
18938893818 <765766546@qq.com>
2023-11-30 00:29:04 +0800

    gfdh

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ const themeConfig = require('./config/themeConfig.js');
 module.exports = {
   theme: 'vdoing', // 使用npm包主题
   title: "vdoing",
-  description: 'vdoing博客主题模板',
+  description: '分享IT技术、承接系统软件开发、提供软件视频教程。',
   base: '/vdoing/', // 格式：'/<仓库名>/'， 默认'/'
   markdown: {
     lineNumbers: true, // 代码行号
